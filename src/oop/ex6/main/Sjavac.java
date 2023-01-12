48f601a03ba680871c0a15b74999ddc4a57f8e9c
roiplatzky <roi.platzky@mail.huji.ac.il>
2023-01-12 11:42:05 +0200

    work on main

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -1,7 +1,6 @@
 package oop.ex6.main;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
@@ -9,43 +8,58 @@ import java.util.regex.Pattern;
 
 public class Sjavac {
 
+    //    private static final String VAR_INITIALIZE_LINE = "^\\s*(?:int|double|String|boolean|char)\\s*" +
+    //                                                      "\\w+\\s*=?,?+;$";
+
+    //regex string pattern:
     private static final String INVALID_FILE_ERR_MSG = "Invalid file name";
     private static final String BLANK_OR_COMMENT = "^//.*$|\\s*";
-    private static final String END_OF_LINE_MODIFIERS = "^.*(?:;|\\{)$"; //TODO: maybe unnecessary
-    private static final String GENERAL_VAR_LINE = "^\\s*(?:(?:int|double|String|boolean|char)(?:\\s*" +
-            "[a-zA-z_\\d]+\\s*=?,?)+|[a-zA-z_\\d]\\s*=\\s*.*?);$";
-    private static final Pattern generalVarLineChecker = Pattern.compile(GENERAL_VAR_LINE);
-    private static final Pattern endOfLineChecker = Pattern.compile(END_OF_LINE_MODIFIERS);
+    private static final String END_OF_LINE_MODIFIERS = "^.*[;{]$"; //TODO: maybe unnecessary
+    private static final String VAR_INITIALIZE_LINE = "^\\s*(?:int|double|String|boolean|char).*;$";
+    private static final String VAR_ASSIGMENT_LINE = "^\\s*[a-zA-z_\\d]\\s*=\\s*.*?;$";
+    private static final String METHOD_LINE = "^\\s*void.*[{]$";
+
+    //pattern declaration:
+    private static final Pattern varInitializeRegex = Pattern.compile(VAR_INITIALIZE_LINE);
+    private static final Pattern endOfLineRegex = Pattern.compile(END_OF_LINE_MODIFIERS);
     private static final Pattern blankOrCommentRegex = Pattern.compile(BLANK_OR_COMMENT);
-    private static Matcher matcher;
+    private static final Pattern varAssignedRegex = Pattern.compile(VAR_ASSIGMENT_LINE);
+    private static final Pattern methodRegex = Pattern.compile(METHOD_LINE);
 
     public static void main(String[] args) {
         String line;
+        Matcher matcher;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]))){
             while ((line = bufferedReader.readLine())!= null){
-                matcher = endOfLineChecker.matcher(line);
+                matcher =  blankOrCommentRegex.matcher(line);
+                if(matcher.matches()) continue;
+                matcher = endOfLineRegex.matcher(line);
                 if(!matcher.matches()){
                     //TODO: check if necessary and if so throw an error
                     System.out.println(1);
                     return;
                 }
-                matcher =  blankOrCommentRegex.matcher(line);
-                if(matcher.matches()) continue;
-                matcher = generalVarLineChecker.matcher(line);
+                matcher = methodRegex.matcher(line);
+                if(matcher.matches()){
+                    //TODO: call method class
+                    continue;
+                }
+                matcher = varInitializeRegex.matcher(line);
                 if(matcher.matches()){
                     //TODO: call variable class
                     continue;
                 }
-
+                matcher = varAssignedRegex.matcher(line);
+                if(matcher.matches()){
+                    //TODO: call variable class
+                    continue;
+                }
+                System.out.println();
+                return ; //TODO: finish
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println(INVALID_FILE_ERR_MSG);
         }
-        catch (IOException e) {
-            System.out.println(INVALID_FILE_ERR_MSG);
-        }
-
     }
 }
 
