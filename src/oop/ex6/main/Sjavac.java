2ba7a9c476c68603b4205be152ab866e6f288f4d
roiplatzky <roi.platzky@mail.huji.ac.il>
2023-01-12 16:07:19 +0200

    main

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -1,5 +1,7 @@
 package oop.ex6.main;
 
+import oop.ex6.main.syntaxVerifier.Variable;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -48,7 +50,7 @@ public class Sjavac {
                 }
                 matcher = varInitializeRegex.matcher(line);
                 if(matcher.matches()){
-                    //TODO: call variable class
+                    Variable.initializeVar(line, 0);
                     continue;
                 }
                 matcher = varAssignedRegex.matcher(line);
