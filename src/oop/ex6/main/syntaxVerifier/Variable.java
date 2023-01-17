308a29bca284768e498f0e4db75a245a12e2efa1
roiplatzky <roi.platzky@mail.huji.ac.il>
2023-01-17 14:43:49 +0200

    nothing

--- src/oop/ex6/main/syntaxVerifier/Variable.java
+++ src/oop/ex6/main/syntaxVerifier/Variable.java
@@ -8,59 +8,6 @@ import java.util.regex.Pattern;
 
 public class Variable {
 
-//    /**
-//     * class that holds the information on the variable
-//     */
-//    private static class VarInfo{
-//
-//        private final String type;
-//        private boolean initialized;
-//        private final boolean isFinal;
-//
-//
-//        /**
-//         * constructor for VarInfo
-//         * @param type - assigned type value
-//         * @param initialized - assigned boolean value
-//         */
-//        public VarInfo(String type, boolean initialized, boolean isFinal){
-//            this.type = type;
-//            this.initialized = initialized;
-//            this.isFinal = isFinal;
-//        }
-//
-//        /**
-//         * returns the type
-//         * @return type value
-//         */
-//        public String getType() {
-//            return type;
-//        }
-//
-//        /**
-//         * returns the initialized argument value
-//         * @return boolean
-//         */
-//        public boolean isInitialized() {
-//            return initialized;
-//        }
-//
-//        /**
-//         * sets the initialized argument to true
-//         */
-//        public void setInitialized(){
-//            initialized = true;
-//        }
-//
-//        /**
-//         * returns if the variable is final
-//         * @return boolean
-//         */
-//        public boolean isFinal() {
-//            return isFinal;
-//        }
-//    }
-
     // Types allowed:
     private static final String INT = "int";
     private static final String DOUBLE = "double";
@@ -69,23 +16,22 @@ public class Variable {
     private static final String CHAR = "char";
     private static final String FINAL = "final";
 
-    //regex txt:
-    private static final String VAR_WITH_INITIALIZE = "^([a-zA-Z]+\\w*|_\\w+)\\s*=\\s*(.*)\\s*$";
-    private static final String VAR_WITHOUT_INITIALIZE = "^([a-zA-Z]+\\w*|_\\w+)\\s*;?\\s*$";
-
     //pattern declaration:
-    private static final Pattern VAR_WITH_INITIALIZE_REGEX = Pattern.compile(VAR_WITH_INITIALIZE);
-    private static final Pattern VAR_WITHOUT_INITIALIZE_REGEX = Pattern.compile(VAR_WITHOUT_INITIALIZE);
+    private static final Pattern VAR_WITH_INITIALIZE_REGEX = Pattern.compile(
+            "^([a-zA-Z]+\\w*|_\\w+)\\s*=\\s*(.*)\\s*$");
+    private static final Pattern VAR_WITHOUT_INITIALIZE_REGEX = Pattern.compile(
+            "^([a-zA-Z]+\\w*|_\\w+)\\s*;?\\s*$");
     private static final Pattern END_REGEX = Pattern.compile("^.*;$");
     private static final Pattern VALUE_IS_INT_REGEX = Pattern.compile("[-+]?\\d+");
     //TODO:maybe add ^ and $ at the beginning and end of the regexes
     private static final Pattern VALUE_IS_STRING_REGEX = Pattern.compile("\".*\"");
     private static final Pattern VALUE_IS_CHAR_REGEX = Pattern.compile("'.'");
-    private static final Pattern VALUE_IS_BOOLEAN_REGEX = Pattern.compile("true|false|[-+]?\\d+|" +
+    private static final Pattern VALUE_IS_BOOLEAN_REGEX = Pattern.compile(
+            "true|false|[-+]?\\d+|[-+]?\\d*[.]\\d+|[-+]?\\d+[.]\\d*");
+    private static final Pattern VALUE_IS_DOUBLE_REGEX = Pattern.compile(
             "[-+]?\\d*[.]\\d+|[-+]?\\d+[.]\\d*");
-    private static final Pattern VALUE_IS_DOUBLE_REGEX = Pattern.compile("[-+]?\\d*[.]\\d+|" +
-            "[-+]?\\d+[.]\\d*");
-    private static final Pattern LEGIT_NAME_REGEX = Pattern.compile("^(?:[a-zA-Z]+\\w*|_\\w+)\\s*$");
+    private static final Pattern LEGIT_NAME_REGEX = Pattern.compile(
+            "^(?:[a-zA-Z]+\\w*|_\\w+)\\s*$");
 
     //other variables:
     private static final ArrayList<HashMap<String, VarInfo>> listOfArgs= new ArrayList<>();
